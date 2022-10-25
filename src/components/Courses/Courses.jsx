573d2c455d66248c6e02988ed9b7f45fd3d03389
Rubel Hossain <rubrex96@hotmail.com>
2022-10-25 14:09:31 +0600

    Courses Componenet UI complete

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,11 +1,84 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { FaReact, FaVuejs, FaAngular, FaNodeJs } from "react-icons/fa";
+import { SiExpress, SiMongodb } from "react-icons/si";
+import { Link, NavLink, useLoaderData } from "react-router-dom";
 
 const Courses = () => {
   const courses = useLoaderData();
+
+  // Icons Lists
+  const icons = [
+    <FaReact className="text-blue-700" />,
+    <FaVuejs className="text-emerald-500" />,
+    <FaAngular className="text-red-700" />,
+    <FaNodeJs className="text-green-700" />,
+    <SiExpress className="text-red-700" />,
+    <SiMongodb className="text-green-700" />,
+  ];
+
   return (
     <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <h2>Total Courses: {courses.length}</h2>
+      <div className="grid grid-cols-12">
+        {/* Sidebar */}
+        <div className="order-2 md:order-1 col-span-12  md:col-span-4 lg:col-span-2">
+          <h2 className="text-2xl mb-5">Total Courses: {courses.length}</h2>
+          <ul className="flex flex-col gap-2 font-bold text-lg">
+            {courses.map((course, index) => (
+              <li key={course._id}>
+                <NavLink
+                  className="flex text-xl items-center justify-center md:justify-start bg-slate-200 hover:bg-blue-200 py-3 px-4 rounded-md"
+                  to={`/courses/${course._id}`}
+                >
+                  <span className="text-3xl">{icons[index]}</span>
+                  <span className="pl-5">{course.course_name}</span>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Details */}
+        <div className="order-1 md:order-2 col-span-12 md:col-span-8 lg:col-span-10 md:border-l-slate-700 md:border-l ml-5">
+          <div className=" md:pl-5">
+            <img
+              src="https://via.placeholder.com/1200x400"
+              alt=""
+              className="mb-10"
+            />
+            <h1 className="text-3xl font-bold text-center py-5 text-slate-800">
+              What is DevSparkle?{" "}
+            </h1>
+            <p className="py-5 text-lg font-medium text-slate-800">
+              We create simplified and interactive learning experiences.
+              Learning web development should be easy to understand and
+              available for everyone, everywhere! DevSparkle is a learning
+              platform for web developers, covering all the aspects of web
+              development.
+              <br />
+              <br />
+              DevSparkle is optimized for learning and training. Examples might
+              be simplified to improve reading and learning. Tutorials,
+              references, and examples are constantly reviewed to avoid errors,
+              but we cannot warrant full correctness of all content.
+            </p>
+            {/* Course Cards */}
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 mt-10 gap-4 ">
+              {/* Card */}
+              {courses.map((course, index) => {
+                return (
+                  <Link
+                    to={`/courses/${course._id}`}
+                    key={course._id}
+                    className="border flex flex-col items-center py-5 hover:bg-blue-200 cursor-pointer"
+                  >
+                    <div className="text-8xl">{icons[index]}</div>
+                    <h2 className="text-3xl">{course.course_name}</h2>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
