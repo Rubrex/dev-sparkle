340fddaef897c8492e81f2d9835aeb88d0f5dcb8
Rubel Hossain <rubrex96@hotmail.com>
2022-10-25 21:35:25 +0600

    LogOut Implemented

--- src/components/Header/NavLinks.jsx
+++ src/components/Header/NavLinks.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { DarkModeToggle } from "react-dark-mode-toggle-2";
+import toast from "react-hot-toast";
 import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
+import { VscAccount } from "react-icons/vsc";
 
 const NavLinks = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const { user, logOut } = useContext(AuthContext);
+
+  // Event handlers
+  const handleLogout = () => {
+    logOut()
+      .then(() => toast.success("Successfully logged out"))
+      .catch(() => toast.error("Something went wrong, failed to log out"));
+  };
   return (
     <>
       <li>
@@ -30,20 +41,40 @@ const NavLinks = () => {
         />
       </li>
       <li>
-        <div className="flex items-center gap-2">
-          <Link
-            to="/Login"
-            className="inline-flex items-center justify-center w-full h-10 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-400 hover:bg-blue-700 focus:shadow-outline focus:outline-none"
-          >
-            Login
-          </Link>
-          <Link
-            to="/register"
-            className="inline-flex items-center justify-center w-full h-10 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-yellow-400 hover:bg-yellow-700 focus:shadow-outline focus:outline-none"
-          >
-            Register
-          </Link>
-        </div>
+        {user ? (
+          <div className="flex items-center gap-2">
+            {user?.photoURL === null ? (
+              <VscAccount className="text-5xl mr-2" />
+            ) : (
+              <img
+                className="w-12 mr-2"
+                src={user?.photoURL}
+                alt="user_profile_picture"
+              />
+            )}
+            <button
+              onClick={handleLogout}
+              className="inline-flex items-center justify-center w-full h-10 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-400 hover:bg-blue-700 focus:shadow-outline focus:outline-none"
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="flex items-center gap-2">
+            <Link
+              to="/Login"
+              className="inline-flex items-center justify-center w-full h-10 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-400 hover:bg-blue-700 focus:shadow-outline focus:outline-none"
+            >
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="inline-flex items-center justify-center w-full h-10 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-yellow-400 hover:bg-yellow-700 focus:shadow-outline focus:outline-none"
+            >
+              Register
+            </Link>
+          </div>
+        )}
       </li>
     </>
   );
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -3,15 +3,21 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { useState } from "react";
 
 export const AuthContext = createContext();
 
-const auth = getAuth(app);
-
 const AuthProvider = ({ children }) => {
+  const auth = getAuth(app);
   // States
+
+  const [user, setUser] = useState();
 
   // Signup with email and password
 
@@ -25,8 +31,27 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  // Logout user
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  // auth Observer
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   // context values
-  let contextValue = { signUp, signIn };
+  let contextValue = { user, signUp, signIn, logOut };
 
   return (
     <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
