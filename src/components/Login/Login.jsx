b99c6ec991bf4cc4299b59b5f4fb474528fb2a3e
Rubel Hossain <rubrex96@hotmail.com>
2022-10-25 21:06:52 +0600

    firebase login inplemented with email/password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useContext } from "react";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const Login = () => {
+  // Context
+  const { signIn } = useContext(AuthContext);
+
+  // Event handlers
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signIn(email, password)
+      .then((res) => {
+        console.log(res);
+        toast.success("Login successful");
+      })
+      .catch((err) => toast.error("Login Failed, Try again."));
+  };
+
   return (
-    <div classNameName=" px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
+    <div className=" px-4  mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
       <div className="w-full max-w-xs mx-auto my-10 ">
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <h2 className="text-center text-2xl font-medium ">Login</h2>
           <div className="form-control">
             <label htmlFor="email">Email</label>
@@ -24,6 +45,12 @@ const Login = () => {
               id="password"
             />
           </div>
+          <span className="text-xs my-2">
+            Don't have an account yet?{" "}
+            <Link to="/register" className="">
+              Sign Up
+            </Link>
+          </span>
           <button type="submit" className="bg-blue-500 text-white">
             Login
           </button>
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,5 +1,9 @@
 import React, { createContext } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -9,14 +13,20 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   // States
 
-  // Signin with email and password
+  // Signup with email and password
 
   const signUp = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  // Signin with email and password
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
   // context values
-  let contextValue = { signUp };
+  let contextValue = { signUp, signIn };
 
   return (
     <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
